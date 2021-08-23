f5568524caacca3ee5b1a629b1ec78fca0e90af3
iazaidi7 <imran.abbas.zaidi@intel.com>
2021-08-23 14:50:19 +0530

    Update pre-build-script.sh

--- examples/pre-build-script.sh
+++ examples/pre-build-script.sh
@@ -4,4 +4,4 @@
 # SPDX-License-Identifier: Apache-2.0
 # ==============================================================================
 curl -L "https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz" |
-tar -C examples/data -xz
+tar -C data -xz
