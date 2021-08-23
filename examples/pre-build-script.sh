8adf15aad940d922d07de03f7cb7bb58886e0aee
iazaidi7 <imran.abbas.zaidi@intel.com>
2021-08-23 14:04:39 +0530

    Update pre-build-script.sh

--- examples/pre-build-script.sh
+++ examples/pre-build-script.sh
@@ -5,6 +5,3 @@
 # ==============================================================================
 curl -L "https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz" |
 tar -C examples/data -xz
-
-deactivate
-rm -rf temp_build
