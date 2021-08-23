3eaa2d5ecb9eb6745575e70159fae56335b6d0ee
iazaidi7 <imran.abbas.zaidi@intel.com>
2021-08-23 13:52:33 +0530

    Update run.sh

--- examples/run.sh
+++ examples/run.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# ==============================================================================
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+# ==============================================================================
 python3 object_detection_sample_video.py --input=data/people-detection.mp4
